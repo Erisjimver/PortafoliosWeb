6403c88a2fd69d419898def4575798249e1b6aca
erisjimver <erisjinver@gmail.com>
2020-10-18 16:23:10 -0500

    correcion de evento, ya los ojos pueden ver hacia abajo

--- EyesFollow/js/scripts.js
+++ EyesFollow/js/scripts.js
@@ -6,7 +6,7 @@ function eyeball()
     eye.forEach(function(eye)
     {
         let x = (eye.getBoundingClientRect().left ) + (eye.clientWidth / 2);
-        let y = (eye.getBoundingClientRect().left ) + (eye.clientHeight / 2);
+        let y = (eye.getBoundingClientRect().top ) + (eye.clientHeight / 2);
 
         let radian = Math.atan2(event.pageX -x, event.pageY -y);
         let rotation = (radian * (180 / Math.PI) * -1) +270;
